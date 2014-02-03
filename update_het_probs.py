c378d13f30ef156d1fbb507a46603e49d6602d20
Bryce van de Geijn <bmvdgeijn@gmail.com>
2014-02-03 16:59:44 -0600

    Added script to update heterozygote probabilities based on all data types

---
+++ update_het_probs.py
@@ -0,0 +1,62 @@
+import genome.db
+
+def main():
+    error=0.01
+    args=parse_options()
+    infile=open(args.infile,,"r")
+    outfile=open(args.outfile,,"w")
+
+    gdb = genome.db.GenomeDB()
+    ref_track=gdb.open_track("10_IND/all_counts/ref_counts_%s" % ind)
+    alt_track=gdb.open_track("10_IND/all_counts/alt_counts_%s" % ind)
+
+    snp_line=infile.read_line()
+    if snp_line:
+        outfile.write(snp_line)
+    else:
+        sys.stderr.write("The input file was empty.\n")
+        exit()
+
+    snp_line=infile.read_line()
+    while snp_line:
+        snpinfo=snpline.strip().split()
+        new_hetps=process_one_snp(snpinfo, ref_track, alt_track,error)
+        outfile.write("\t".join(snpinfo[:10]+";".join(new_hetps)+snpinfo[11:])+"\n")
+        snp_line=infile.read_line()
+    
+
+def process_one_snp(snpinfo, ref_track, alt_track,error):
+    chrm=snpinfo[0]
+    # positions of target SNPs
+    snplocs=[int(y.strip()) for y in snpinfo[9].split(';')]
+    # heterozygote probabilities of target SNPs
+    hetps = [float(y.strip()) for y in snpinfo[10].split(';')]
+    update_hetps=[]
+    for i in range(len(snplocs)):
+        pos=snplocs[i]
+        adr=ref_track.get_nparray(chrm, pos, pos)[0]
+        ada=alt_track.get_nparray(chrm, pos, pos)[0]
+        update_hetps.append(get_posterior_hetp(hetps[i],adr,ada,error))
+    return update_hetps
+
+
+def get_posterior_hetp(hetp_prior,adr,ada,error):
+    prior = min(.99,hetp_prior)
+    badlike=addlogs(math.log(error)*adr+math.log(1-error)*ada,math.log(1-error)*adr+math.log(error)*ada)
+    goodlike=math.log(0.5)*adr+math.log(0.5)*ada
+    if goodlike-badlike > 40:
+        return 1
+    else:
+        return prior*math.exp(goodlike-badlike)/(prior*math.exp(goodlike-badlike)+(1-prior))
+        
+def addlogs(loga,logb):
+    return max(loga,logb)+math.log(1+math.exp(-abs(loga-logb)))
+
+def parse_options():
+    parser=argparse.ArgumentParser()
+    parser.add_argument("infile", action='store', default=None)
+    parser.add_argument("outfile", action='store', default=None)
+    parser.add_argument("reffile", action='store', default=None)
+    parser.add_argument("altfile", action='store', default=None)
+    
+    return parser.parse_args()

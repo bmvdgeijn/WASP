f850b49d1dfb81014e46899685a0f660af71bee0
Christopher DeBoever <cdeboever3@gmail.com>
2015-08-06 18:37:21 -0700

    Refactoring, PEP8, new test file for filter
    I've added a file with tests for filter_remapped_reads.py but it's not
    ready to be used yet. I'm still figuring out how that script works,
    constructing test data, and designing the tests.

--- mapping/filter_remapped_reads.py
+++ mapping/filter_remapped_reads.py
@@ -1,4 +1,4 @@
-def run(orig_bam, remap_bam, keep_bam, orig_num_file):
+def run(orig_bam, remap_bam, keep_bam, orig_num_file, is_paired_end):
     import gzip
     import sys
 
@@ -9,22 +9,42 @@ def run(orig_bam, remap_bam, keep_bam, orig_num_file):
     keep_bam = pysam.Samfile(keep_bam, "wb", template=orig_bam)
     orig_num_file = gzip.open(orig_num_file)
 
+    # correct_maps is a list of read pairs that mapped correctly. The read pair
+    # is represented by its number (which is the order that it was seen in the
+    # original input bam file and also the first number in the its new read
+    # name, e.g. the 42 in @42:chr1:14536:1.
     correct_maps = []
     end_of_file = False
     
     # Get a list of reads that remapped correctly.
     remap_read = remap_bam.next()
     
-    while not end_of_file:    
+    while not end_of_file:
+        # The read names consist of four or five parts (depending on single or
+        # paired end data) separated by colons. The first part is the remap
+        # number. The first read pair from the input file to be remapped is 1,
+        # the second read pair is 2, etc. The second part is the chromosome
+        # name. The third part is the alignment position of the original read
+        # pair. The last part is the number of alternative sequences for the
+        # read pair (i.e. the number of new sequences with swapped alleles we
+        # have to map to see if they match with the alignment of the original
+        # sequence).  Note that if a read pair overlaps multiple SNPs and has
+        # multiple alternate sequences, each of those alternate sequences will
+        # have the exact same read name.
         chrm = remap_read.qname.strip().split(":")[1]
+        
         if remap_read.is_reverse:
+            # TODO: This works when you have paired end data but I feel like
+            # this could be a problem for single end data. If the read was
+            # mapped to the reverse strand its position would be compared to the
+            # number of alternative sequences.
             pos = int(remap_read.qname.strip().split(":")[3])
         else:
             pos = int(remap_read.qname.strip().split(":")[2])
         read_num = int(remap_read.qname.strip().split(":")[0])
         if (remap_read.tid != -1 and remap_read.pos == pos and 
             remap_bam.getrname(remap_read.tid) == chrm):
-            dels = 0   
+            dels = 0
             # Throw out the remapped read if it remapped with a deletion...for
             # now.
             for cig in remap_read.cigar:
@@ -38,13 +58,14 @@ def run(orig_bam, remap_bam, keep_bam, orig_num_file):
             end_of_file = True
     
     correct_maps.sort()
-    
-    sys.stderr.write(str(len(correct_maps)) + 
-                     " reads remapped to the correct position\n")
-    
+    sys.stderr.write('{:,} reads remapped to the correct position\n'.format(
+        len(correct_maps)))
+
     # Pull out original aligned reads if all of the alternatives mapped
     # correctly.
     orig_read = orig_bam.next()
+    # I believe orig_num is the number of different read pairs generated from
+    # the original read pair (depends on number of alleles it overlapped).
     orig_num = int(orig_num_file.readline().strip())
     line_num = 1
     
@@ -52,16 +73,19 @@ def run(orig_bam, remap_bam, keep_bam, orig_num_file):
     correct = 0
     end_of_file = False
     
+    import pdb
+    pdb.set_trace()
     while (not end_of_file and map_indx < len(correct_maps) and 
            line_num <= correct_maps[-1]):
         if line_num < correct_maps[map_indx]:
             if orig_num == correct:
                 keep_bam.write(orig_read)
-            if options.is_paired_end:
+            # If the data is paired end, write out the paired read.
+            if is_paired_end:
                 try:
                     orig_read = orig_bam.next()
                 except:
-                    sys.stderr.write("File ended unexpectedly (no pair found)")
+                    sys.stderr.write("File ended unexpectedly (no pair found).")
                     exit()
                 if orig_num == correct:
                     keep_bam.write(orig_read)
@@ -77,22 +101,28 @@ def run(orig_bam, remap_bam, keep_bam, orig_num_file):
             correct += 1
             map_indx += 1
         else:
-            sys.stderr.write("There was a problem with the index sorting\n")
+            sys.stderr.write("There was a problem with the index sorting\n.")
             exit()
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", action='store_true', dest='is_paired_end', default=False)
-    parser.add_argument("orig_bam")
-    parser.add_argument("remap_bam")
-    parser.add_argument("keep_bam")
-    parser.add_argument("orig_num_file")
+    parser.add_argument("-p", action='store_true', dest='is_paired_end', 
+                        default=False, help=('Indicates that reads are '
+                                             'paired-end (default is single).'))
+    h = ('to.remap.bam file from find_intersecting_snps.py.')
+    parser.add_argument("to_remap_bam", help=h)
+    parser.add_argument("remap_bam", help='Remapped bam file.')
+    parser.add_argument("keep_bam", help=('File to write correctly remapped '
+                                          'reads to.'))
+    h = 'to.remap.num.gz file from find_intersecting_snps.py.'
+    parser.add_argument("orig_num_file", help=h)
     
-    options= parser.parse_args()
+    options = parser.parse_args()
     
-    run(orig_bam, remap_bam, keep_bam, orig_num_file)
+    run(options.orig_bam, options.remap_bam, options.keep_bam,
+        options.orig_num_file, options.is_paired_end)
 
 if __name__ == '__main__':
     main()
---
+++ mapping/test_filter_remapped_reads.py
@@ -0,0 +1,131 @@
+import glob
+import gzip
+import os
+import subprocess
+
+from find_intersecting_snps import *
+from filter_remapped_reads import *
+
+def read_bam(bam):
+    """
+    Read a bam file into a list where each element of the list is a line from
+    the bam file (with the newline stripped). The header is discarded.
+    """
+    res = subprocess.check_output('samtools view {}'.format(bam), shell=True)
+    return res.strip().split('\n')
+
+def cleanup():
+    fns = (glob.glob('test_data/test*.keep.bam') +
+           glob.glob('test_data/test*.remap.fq*.gz') + 
+           glob.glob('test_data/test*.to.remap.bam') + 
+           glob.glob('test_data/test*.to.remap.num.gz'))
+    [os.remove(x) for x in fns]
+
+class TestRun:
+    # def test_simple_single(self):
+    #     is_paired_end = False
+    #     max_window = 100000
+    #     pref = 'test_data/test_single'
+    #     file_name = pref + ".sort.bam"
+    #     keep_file_name = pref + ".keep.bam"
+    #     remap_name = pref + ".to.remap.bam"
+    #     remap_num_name = pref + ".to.remap.num.gz"
+    #     fastq_names = [pref + ".remap.fq.gz"]
+    #     snp_dir = 'test_data/snps'
+    #     bs = BamScanner(is_paired_end, max_window, file_name, keep_file_name,
+    #                     remap_name, remap_num_name, fastq_names, snp_dir)
+    #     bs.run()
+
+    #     keep_bam = pref + '_filtered.bam'
+    #     run(remap_name, 'test_data/test_single.remapped.bam', keep_bam,
+    #         remap_num_name, is_paired_end)
+
+    #     cleanup()
+    
+    def test_simple_paired(self):
+        is_paired_end = True
+        max_window = 100000
+        pref = 'test_data/test_paired'
+        file_name = pref + ".sort.bam"
+        keep_file_name = pref + ".keep.bam"
+        remap_name = pref + ".to.remap.bam"
+        remap_num_name = pref + ".to.remap.num.gz"
+        fastq_names = [pref + ".remap.fq1.gz",
+                       pref + ".remap.fq2.gz"]
+        snp_dir = 'test_data/snps'
+        bs = BamScanner(is_paired_end, max_window, file_name, keep_file_name,
+                        remap_name, remap_num_name, fastq_names, snp_dir)
+        bs.run()
+        
+        # keep_bam = pref + '_filtered.bam'
+        # run(remap_name, 'test_data/test_paired.remapped.bam', keep_bam,
+        #     remap_num_name, is_paired_end)
+
+        # cleanup()
+
+#     def test_two_snps_single(self):
+#         is_paired_end = False
+#         max_window = 100000
+#         pref = 'test_data/test_single'
+#         file_name = pref + ".sort.bam"
+#         keep_file_name = pref + ".keep.bam"
+#         remap_name = pref + ".to.remap.bam"
+#         remap_num_name = pref + ".to.remap.num.gz"
+#         fastq_names = [pref + ".remap.fq.gz"]
+#         snp_dir = 'test_data/two_snps'
+#         bs = BamScanner(is_paired_end, max_window, file_name, keep_file_name,
+#                         remap_name, remap_num_name, fastq_names, snp_dir)
+#         bs.run()
+# 
+#         keep_bam = pref + '_filtered.bam'
+#         run(remap_name, 'test_data/test_single_two_snps.remapped.bam', keep_bam,
+#             remap_num_name, is_paired_end)
+# 
+#         cleanup()
+# 
+#     def test_two_snps_paired(self):
+#         is_paired_end = True
+#         max_window = 100000
+#         pref = 'test_data/test_paired'
+#         file_name = pref + ".sort.bam"
+#         keep_file_name = pref + ".keep.bam"
+#         remap_name = pref + ".to.remap.bam"
+#         remap_num_name = pref + ".to.remap.num.gz"
+#         fastq_names = [pref + ".remap.fq1.gz",
+#                        pref + ".remap.fq2.gz"]
+#         snp_dir = 'test_data/two_snps'
+#         bs = BamScanner(is_paired_end, max_window, file_name, keep_file_name,
+#                         remap_name, remap_num_name, fastq_names, snp_dir)
+#         bs.run()
+#         
+#         keep_bam = pref + '_filtered.bam'
+#         run(remap_name, 'test_data/test_paired_two_snps.remapped.bam', keep_bam,
+#             remap_num_name, is_paired_end)
+# 
+#         cleanup()
+# 
+#     def test_2015_06_18_bug(self):
+#         """
+#         This was reported as a bug because one read pair that overlaps one SNP
+#         was resulting in multiple pairs of reads in the fastq files. However, it
+#         is not a bug because the reads overlap and both reads overlap the SNP.
+#         """
+#         is_paired_end = True
+#         max_window = 100000
+#         file_name = 'test_data/2015_06_18_bug.bam'
+#         pref = 'test_data/test_paired'
+#         keep_file_name = pref + ".keep.bam"
+#         remap_name = pref + ".to.remap.bam"
+#         remap_num_name = pref + ".to.remap.num.gz"
+#         fastq_names = [pref + ".remap.fq1.gz",
+#                        pref + ".remap.fq2.gz"]
+#         snp_dir = 'test_data/2015_06_18_bug_snps'
+#         bs = BamScanner(is_paired_end, max_window, file_name, keep_file_name,
+#                         remap_name, remap_num_name, fastq_names, snp_dir)
+#         bs.run()
+#         
+#         keep_bam = pref + '_filtered.bam'
+#         run(remap_name, 'test_data/test_2015_06_18_bug.remapped.bam', keep_bam,
+#             remap_num_name, is_paired_end)
+# 
+#         cleanup()

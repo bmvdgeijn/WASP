2bef16af3411ed21ef88784b28e23d2c7e40d5af
Graham McVicker <gmcvicker@salk.edu>
2017-11-16 09:47:09 -0800

    updates to power analysis scripts

--- CHT/power_analysis/cht_power_analysis.R
+++ CHT/power_analysis/cht_power_analysis.R
@@ -6,7 +6,6 @@ power.sim.tab <- read.table(paste(base.dir, "/power_sim_params.txt", sep=""), he
 tab <- power.sim.tab
 tab$N.SIGNIF <- rep(NA, nrow(tab))
 tab$N.TEST <- rep(NA, nrow(tab))
-tab$FRAC.SIGNIF <- rep(NA, nrow(tab))
 
 for(i in seq(1, nrow(tab))) {
   ref.af <- tab$REF.ALLELE.FREQ[i]
@@ -27,27 +26,63 @@ for(i in seq(1, nrow(tab))) {
     n.test <- nrow(combined.tab)
     expect.p <- seq(1, n.test)/(1 + n.test)
     obs.p <- combined.tab$P.VALUE
+    obs.p[obs.p == 0] <- min(obs.p[obs.p != 0])
     #qqplot(-log10(expect.p), -log10(obs.p))
-    #abline(a=0, b=1, col="red")
-
+    
+    test.name <- as.character(combined.tab$TEST.SNP.ID)
+    is.alt <- startsWith(test.name, "ALT")
+    is.null <- !is.alt
+    
+    # ord <- order(obs.p)
+    # clr <- rep(NA, length(obs.p))
+    # clr[is.alt] <- "red"
+    # clr[!is.alt] <- "blue"
+    # plot(-log10(expect.p), -log10(obs.p[ord]), col=clr[ord])
+    # abline(a=0, b=1, col="red")
+    
     # use Benjamini-Hochberg method to calculate FDR
     fdr <- p.adjust(obs.p, method="BH")
-
+    
+    n.alt <- sum(is.alt)
+    n.null <- sum(is.null)
+    
     # what p-value corresponds to FDR 5%?
     if(any(fdr < 0.05)) {
       p.thresh <- max(obs.p[fdr <= 0.05])
       # lines(x=c(0, log10(n.test)), y=rep(-log10(p.thresh), 2), col="blue", lty=2)
-
+      
       n.signif <- sum(obs.p <= p.thresh)
       frac.signif <- n.signif / n.test
+      
+      n.alt.signif <- sum((obs.p <= p.thresh) & is.alt)
+      n.null.signif <- sum((obs.p <= p.thresh) & is.null)
+    
+      # true positive rate (sensitivity)
+      tp.rate <- n.alt.signif / n.alt
+      # true negative rate (specificity)
+      tn.rate <- (n.null - n.null.signif) / n.null
+      # false positive rate
+      fp.rate <- n.null.signif / n.null
+      
+      # observed FDR
+      obs.fdr <- n.null.signif / (n.null.signif + n.alt.signif)
+      
     } else {
       n.signif <- 0
+      n.alt.signif <- 0
+      n.null.signif <- 0
       frac.signif <- 0.0
+      tp.rate <- 0
+      tn.rate <- 1.0
+      obs.fdr <- 0.0
     }
   
-      tab$N.SIGNIF[i] <- n.signif
-      tab$N.TEST[i] <- n.test
-      tab$FRAC.SIGNIF[i] <- frac.signif
+    tab$N.SIGNIF[i] <- n.signif
+    tab$N.TEST[i] <- n.test
+    tab$N.TP[i] <- n.alt.signif
+    tab$N.TN[i] <- n.null - n.null.signif
+    tab$TP.RATE[i] <- tp.rate
+    tab$TN.RATE[i] <- tn.rate
   }
 }
 
@@ -68,16 +103,16 @@ for(j in 1:length(samp.sizes)) {
       tab$REF.ALLELE.FREQ <= 0.5
   
   if(j == 1) {
-      plot(tab$REF.ALLELE.FREQ[f], tab$FRAC.SIGNIF[f], col=col.pal[j], ylim=c(0,1),
-           type="o", las=1, ylab="prob signif test at FDR < 0.05", pch=20, 
-           main=paste("log2(effect size)=0.26", sep=""), xlab="minor allele freq")
+      plot(tab$REF.ALLELE.FREQ[f], tab$TP.RATE[f], col=col.pal[j], ylim=c(0,1),
+           type="o", las=1, ylab="True Positive Rate (FDR < 0.05)", pch=20, 
+           main=paste("log2(effect size)=0.26", sep=""), xlab="Minor Allele Freq")
       
       legend("bottomright", legend=paste("n=", samp.sizes, sep=""), 
              pch=20, col=col.pal, title="sample size")
     } else {
-      points(tab$REF.ALLELE.FREQ[f], tab$FRAC.SIGNIF[f], pch=20, col=col.pal[j], type="o")
+      points(tab$REF.ALLELE.FREQ[f], tab$TP.RATE[f], pch=20, col=col.pal[j], type="o")
     }
-  }
+}
 
 
 for(i in 1:length(samp.sizes)) {
@@ -87,14 +122,14 @@ for(i in 1:length(samp.sizes)) {
   new.tab <- new.tab[order(new.tab$EFFECT.SIZE),]
   
   if(i == 1) {
-    plot(log2(new.tab$EFFECT.SIZE), new.tab$FRAC.SIGNIF, col=col.pal[i], ylim=c(0,1),
-         type="o", las=1, ylab="prob signif test at FDR < 0.05", pch=20, 
+    plot(log2(new.tab$EFFECT.SIZE), new.tab$TP.RATE, col=col.pal[i], ylim=c(0,1),
+         type="o", las=1, ylab="True Positive Rate (FDR < 0.05)", pch=20, 
          main=paste("minor allele freq=", 0.2, sep=""), xlab="log2(effect size)")
     
     legend("bottomright", legend=paste("n=", samp.sizes, sep=""), 
            pch=20, col=col.pal, title="sample size")
   } else {
-    points(log2(new.tab$EFFECT.SIZE), new.tab$FRAC.SIGNIF, pch=20, col=col.pal[i], type="o")
+    points(log2(new.tab$EFFECT.SIZE), new.tab$TP.RATE, pch=20, col=col.pal[i], type="o")
   }
 }
 
@@ -102,5 +137,47 @@ dev.off()
 
 
 
+
+pdf("cht_specificity.pdf", width=5, height=10)
+
+par(mfrow=(c(2,1)))
+
+for(j in 1:length(samp.sizes)) {
+  f <- tab$EFFECT.SIZE == 1.2 & 
+    tab$SAMPLE.SIZE == samp.sizes[j] & 
+    tab$REF.ALLELE.FREQ <= 0.5
+  
+  if(j == 1) {
+    plot(tab$REF.ALLELE.FREQ[f], tab$TN.RATE[f], col=col.pal[j], ylim=c(0,1),
+         type="o", las=1, ylab="True Negative Rate (FDR < 0.05)", pch=20, 
+         main=paste("log2(effect size)=0.26", sep=""), xlab="Minor Allele Freq")
+    
+    legend("bottomright", legend=paste("n=", samp.sizes, sep=""), 
+           pch=20, col=col.pal, title="sample size")
+  } else {
+    points(tab$REF.ALLELE.FREQ[f], tab$TN.RATE[f], pch=20, col=col.pal[j], type="o")
+  }
+}
+
+
+for(i in 1:length(samp.sizes)) {
+  f <- tab$SAMPLE.SIZE == samp.sizes[i] & tab$REF.ALLELE.FREQ == 0.20 & abs(log2(tab$EFFECT.SIZE)) <= 1
+  
+  new.tab <- tab[f,]
+  new.tab <- new.tab[order(new.tab$EFFECT.SIZE),]
+  
+  if(i == 1) {
+    plot(log2(new.tab$EFFECT.SIZE), new.tab$TN.RATE, col=col.pal[i], ylim=c(0,1),
+         type="o", las=1, ylab="True Negative Rate (FDR < 0.05)", pch=20, 
+         main=paste("minor allele freq=", 0.2, sep=""), xlab="log2(effect size)")
+    
+    legend("bottomright", legend=paste("n=", samp.sizes, sep=""), 
+           pch=20, col=col.pal, title="sample size")
+  } else {
+    points(log2(new.tab$EFFECT.SIZE), new.tab$TN.RATE, pch=20, col=col.pal[i], type="o")
+  }
+}
+
+dev.off()
 
 
